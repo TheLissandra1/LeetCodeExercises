cfdc99f047dd131c75947678f68e1cfd4ff09903
Nisha Q <n.qiao@students.uu.nl>
2024-01-05 19:31:23 +0100

    Create 9. Palindrome.cs

---
+++ C#/9. Palindrome.cs
@@ -0,0 +1,42 @@
+// 回文数 Palindrome
+Solution 1:
+
+public class Solution {
+    // 暴力转字符串反转对比
+    public bool IsPalindrome(int x) {
+        String x1 = new StringBuilder(x + "").ToString();
+        char[] charArray = x1.ToCharArray();
+        Array.Reverse(charArray);
+        String reversedStr = new string(charArray);
+        return x1.Equals(reversedStr);
+
+    }
+}
+
+
+Solution 2:
+
+// 取整取余折叠一半数字
+public class Solution {
+    public bool IsPalindrome(int x) {
+        // 特殊情况：
+        // 如上所述，当 x < 0 时，x 不是回文数。
+        // 同样地，如果数字的最后一位是 0，为了使该数字为回文，
+        // 则其第一位数字也应该是 0
+        // 只有 0 满足这一属性
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+
+        // 当数字长度为奇数时，我们可以通过 revertedNumber/10 去除处于中位的数字。
+        // 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
+        // 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+}
